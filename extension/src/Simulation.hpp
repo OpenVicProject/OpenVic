d45323cadee9044d8536f7eb9827a43b36e4bdff
Robert Clarke <clarke.john.robert@gmail.com>
2023-02-12 03:44:39 -0500

    Everything necessary to register a C++ class with internal state for access within Godot

---
+++ extension/src/Simulation.hpp
@@ -0,0 +1,52 @@
+#pragma once
+
+#include <godot_cpp/classes/object.hpp>
+#include <godot_cpp/core/class_db.hpp>
+#include <godot_cpp/variant/utility_functions.hpp>
+#include <vector>
+
+namespace OpenVic2 {
+	class Simulation : public godot::Object {
+		std::vector<size_t> exampleProvinces;
+
+		//BEGIN BOILERPLATE
+		GDCLASS(Simulation, godot::Object);
+		static Simulation* _simulation;
+
+		protected:
+		static void _bind_methods() {
+			godot::ClassDB::bind_method(godot::D_METHOD("conductSimulationStep"), &Simulation::conductSimulationStep);
+			godot::ClassDB::bind_method(godot::D_METHOD("queryProvinceSize"), &Simulation::queryProvinceSize);
+		}
+
+		public:
+		inline static Simulation* get_singleton() { return _simulation; }
+
+		inline Simulation() {
+			ERR_FAIL_COND(_simulation != nullptr);
+			_simulation = this;
+
+			exampleProvinces.resize(10, 1);
+		}
+		inline ~Simulation() {
+			ERR_FAIL_COND(_simulation != this);
+			_simulation = nullptr;
+		}
+		//END BOILERPLATE
+
+		inline void conductSimulationStep() {
+			for (size_t x = 0; x < exampleProvinces.size(); x++) {
+				exampleProvinces[x] += (x + 1);
+			}
+		}
+
+		inline size_t queryProvinceSize(size_t provinceID) {
+			if (provinceID >= exampleProvinces.size()) {
+				return 0;
+			}
+			return exampleProvinces[provinceID];
+		}
+	};
+
+	Simulation* Simulation::_simulation = nullptr;
+}
