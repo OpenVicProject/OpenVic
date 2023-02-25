11d509c599744ea6fbb4955767ecef84352f7d17
Joel Machens <ajmach6@gmail.com>
2023-02-25 16:47:37 -0600

    Add req ids to play function.

--- game/src/SFX/SFX.gd
+++ game/src/SFX/SFX.gd
@@ -6,8 +6,9 @@ func _ready():
 	var dir = DirAccess.open("res://audio/sfx/")
 	for fname in dir.get_files():
 		if fname.get_extension() == "ogg":
-			loadedSFX[fname.split(".")[0]] = load("res://audio/sfx/" + fname)
+			loadedSFX[fname.split(".")[0]] = load("res://audio/sfx/" + fname) # SND-10
 
+# SND-7
 func play(sound):
 	var player = AudioStreamPlayer.new()
 	player.bus = "SFX"
