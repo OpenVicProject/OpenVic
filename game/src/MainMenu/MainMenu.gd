900653f61c599babfc9017dd5ef38f3ea7c4fc5e
Joel Machens <ajmach6@gmail.com>
2023-02-25 17:26:02 -0600

    branch with project restructure

--- game/src/MainMenu/MainMenu.gd
+++ game/src/MainMenu/MainMenu.gd
@@ -12,28 +12,30 @@ func _ready():
 
 
 func _on_new_game_button_pressed():
-	await get_node("/root/GameMenu/SFX").play("click")
+	get_node("/root/OpenVic2/SFX").play("click")
 	print("Start a new game!")
-	get_tree().change_scene_to_file("res://src/SampleGame.tscn")
+	get_node("/root/OpenVic2").remove_child(get_node("/root/OpenVic2/GameMenu"))
+	var sample_game = load("res://src/SampleGame.tscn")
+	get_node("/root/OpenVic2").add_child(sample_game)
 
 
 func _on_continue_button_pressed():
-	await get_node("/root/GameMenu/SFX").play("click")
+	get_node("/root/OpenVic2/SFX").play("click")
 	print("Continue last game!")
 
 
 func _on_multi_player_button_pressed():
-	await get_node("/root/GameMenu/SFX").play("click")
+	get_node("/root/OpenVic2/SFX").play("click")
 	print("Have fun with friends!")
 
 
 func _on_options_button_pressed():
-	await get_node("/root/GameMenu/SFX").play("click")
+	get_node("/root/OpenVic2/SFX").play("click")
 	print("Check out some options!")
 	options_button_pressed.emit()
 
 
 func _on_exit_button_pressed():
-	await get_node("/root/GameMenu/SFX").play("click")
+	await get_node("/root/OpenVic2/SFX").play("click")
 	print("See you later!")
 	get_tree().quit()
