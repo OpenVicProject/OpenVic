bd4cc34b2167139635918694bf547d8fba41bb8a
Robert Clarke <clarke.john.robert@gmail.com>
2023-03-01 11:29:56 -0500

    Added selectable songs and player visibility toggle

--- game/src/MusicConductor/MusicConductor.gd
+++ game/src/MusicConductor/MusicConductor.gd
@@ -1,20 +1,27 @@
 extends Node
-
+# SS-67
 var selectedTrack = 0
 var availableSongs : Array[SongInfo] = []
 var autoPlayNextSong : bool = true
 
+func getAllSongNames() -> Array[String]:
+	var songNames : Array[String] = []
+	for si in availableSongs:
+		songNames.append(si.readableName)
+	return songNames
+
+func getCurrentSongIndex() -> int:
+	return selectedTrack
+
 func getCurrentSongName() -> String:
 	return availableSongs[selectedTrack].readableName
 
-func getCurrentSongTime() -> float:
-	return $AudioStreamPlayer.get_playback_position()
-
-func getCurrentSongLength() -> float:
-	return $AudioStreamPlayer.stream.get_length()
+func scrubSongByPercentage(percentage: float) -> void:
+	var percentInSeconds : float = (percentage / 100.0) * $AudioStreamPlayer.stream.get_length()
+	$AudioStreamPlayer.play(percentInSeconds)
 
 func getCurrentSongProgressPercentage() -> float:
-	return 100 * (getCurrentSongTime() / getCurrentSongLength())
+	return 100 * ($AudioStreamPlayer.get_playback_position() / $AudioStreamPlayer.stream.get_length()) #(getCurrentSongTime() / getCurrentSongLength())
 
 func isPaused() -> bool:
 	return $AudioStreamPlayer.stream_paused
@@ -26,6 +33,12 @@ func startCurrentSong() -> void:
 	$AudioStreamPlayer.stream = availableSongs[selectedTrack].loadedStream
 	$AudioStreamPlayer.play()
 
+# SS-70
+func startSongByIndex(id: int) -> void:
+	selectedTrack = id
+	startCurrentSong()
+
+# SS-69
 func nextSong() -> void:
 	selectedTrack = (selectedTrack + 1) % len(availableSongs)
 	startCurrentSong()
--- game/src/MusicConductor/MusicUIController.gd
+++ game/src/MusicConductor/MusicUIController.gd
@@ -1,17 +1,23 @@
 extends Control
+# UI-104
 
+var isMusicPlayerVisible : bool = true
+var isUserDraggingProgressSlider : bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	updateMusicLabel()
+	for songName in MusicConductor.getAllSongNames():
+		$VBoxContainer/SongSelectorButton.add_item(songName, $VBoxContainer/SongSelectorButton.item_count)
+	updateSongNameVisual()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	$VBoxContainer/ProgressSlider.value = MusicConductor.getCurrentSongProgressPercentage()
+	if !isUserDraggingProgressSlider:
+		$VBoxContainer/ProgressSlider.value = MusicConductor.getCurrentSongProgressPercentage()
 
-func updateMusicLabel():
-	$VBoxContainer/MusicNameLabel.text = MusicConductor.getCurrentSongName()
+func updateSongNameVisual():
+	$VBoxContainer/SongSelectorButton.selected = MusicConductor.getCurrentSongIndex()
 
 func updatePlayPauseButtonVisual():
 	$VBoxContainer/HBoxContainer/PlayPauseButton.text = "||" if MusicConductor.isPaused() else ">"
@@ -22,10 +28,32 @@ func _on_play_pause_button_pressed():
 
 func _on_next_song_button_pressed():
 	MusicConductor.nextSong()
-	updateMusicLabel()
+	updateSongNameVisual()
 	updatePlayPauseButtonVisual()
 
 func _on_previous_song_button_pressed():
 	MusicConductor.prevSong()
-	updateMusicLabel()
+	updateSongNameVisual()
 	updatePlayPauseButtonVisual()
+
+# UI-107
+func _on_option_button_item_selected(index):
+	# UIFUN-92
+	MusicConductor.startSongByIndex(index)
+
+
+func _on_progress_slider_drag_started():
+	isUserDraggingProgressSlider = true
+
+
+func _on_progress_slider_drag_ended(_value_changed):
+	MusicConductor.scrubSongByPercentage($VBoxContainer/ProgressSlider.value)
+	isUserDraggingProgressSlider = false
+
+# UI-107
+func _on_music_ui_visibility_button_pressed():
+	isMusicPlayerVisible = !isMusicPlayerVisible
+	$VBoxContainer/MusicUIVisibilityButton.text = "Hide Player" if isMusicPlayerVisible else "Show Player"
+	$VBoxContainer/SongSelectorButton.visible = isMusicPlayerVisible
+	$VBoxContainer/ProgressSlider.visible = isMusicPlayerVisible
+	$VBoxContainer/HBoxContainer.visible = isMusicPlayerVisible
