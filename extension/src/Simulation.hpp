48558fdfec509942515b0e2d92e3f1357b201400
markomitos <markomitosevic9@gmail.com>
2023-03-01 23:20:58 +0100

    Add Date to extension 	For absolute simulation dates
    Add TimeSpan to extension
    	For relative simulation time
    
    Add Simulation Speed Management to extension
    Add Visual Studio directory to .gitignore
    
    Co-authored-by: ClarkeCode <clarke.john.robert@gmail.com>
    Co-authored-by: Hop311 <hop3114@gmail.com>

--- extension/src/Simulation.hpp
+++ extension/src/Simulation.hpp
@@ -2,28 +2,39 @@
 
 #include <godot_cpp/core/class_db.hpp>
 #include <vector>
+#include <chrono>
+#include "openvic2/Date.hpp"
 
 namespace OpenVic2 {
 	class Simulation : public godot::Object {
 		GDCLASS(Simulation, godot::Object)
 		std::vector<uint64_t> exampleProvinces;
 
+		enum class Speed { Speed1 = 4000, Speed2 = 3000, Speed3 = 2000, Speed4 = 1000, Speed5 = 100, Speed6 = 1 };
+
+		std::chrono::time_point<std::chrono::high_resolution_clock> lastPolledTime;
+		bool isPaused;
+		Speed currentSpeed;
+		Date inGameDate;
+
 		//BEGIN BOILERPLATE
 		inline static Simulation* _simulation = nullptr;
 
-		protected:
+	protected:
 		static void _bind_methods() {
 			godot::ClassDB::bind_method(godot::D_METHOD("conductSimulationStep"), &Simulation::conductSimulationStep);
 			godot::ClassDB::bind_method(godot::D_METHOD("queryProvinceSize"), &Simulation::queryProvinceSize);
 		}
 
-		public:
+	public:
 		inline static Simulation* get_singleton() { return _simulation; }
 
-		inline Simulation() {
+		inline Simulation() : inGameDate(1836, 1, 1) {
 			ERR_FAIL_COND(_simulation != nullptr);
 			_simulation = this;
-
+			this->lastPolledTime = std::chrono::high_resolution_clock::now();
+			this->isPaused = false;
+			this->currentSpeed = Speed::Speed1;
 			exampleProvinces.resize(10, 1);
 		}
 		inline ~Simulation() {
@@ -31,6 +42,13 @@ namespace OpenVic2 {
 			_simulation = nullptr;
 		}
 		//END BOILERPLATE
+
+		void togglePauseState();
+		bool getPauseState();
+		void increaseSimulationSpeed();
+		void decreaseSimulationSpeed();
+		void setSimulationSpeed(Speed speed);
+		int getSimulationSpeed();
 
 		inline void conductSimulationStep() {
 			for (uint64_t x = 0; x < exampleProvinces.size(); x++) {
@@ -44,5 +62,7 @@ namespace OpenVic2 {
 			}
 			return exampleProvinces[provinceID];
 		}
+
+		void conditionallyAdvanceSimulation();
 	};
 }
---
+++ extension/src/openvic2/Date.cpp
@@ -0,0 +1,108 @@
+#include <sstream>
+#include "Date.hpp"
+
+namespace OpenVic2 {
+	bool Timespan::operator<  (Timespan const& other) const { return days <  other.days; }
+	bool Timespan::operator>  (Timespan const& other) const { return days >  other.days; }
+	bool Timespan::operator<= (Timespan const& other) const { return days <= other.days; }
+	bool Timespan::operator>= (Timespan const& other) const { return days >= other.days; }
+	bool Timespan::operator== (Timespan const& other) const { return days == other.days; }
+	bool Timespan::operator!= (Timespan const& other) const { return days != other.days; }
+
+	Timespan Timespan::operator+ (Timespan const& other) const { return Timespan(days + other.days); }
+	Timespan Timespan::operator- (Timespan const& other) const { return Timespan(days - other.days); }
+	Timespan Timespan::operator* (int64_t const& factor) const { return Timespan(days * factor); }
+	Timespan Timespan::operator/ (int64_t const& factor) const { return Timespan(days / factor); }
+
+	Timespan& Timespan::operator+= (Timespan const& other) {
+		days += other.days;
+		return *this;
+	}
+	Timespan& Timespan::operator-= (Timespan const& other) {
+		days -= other.days;
+		return *this;
+	}
+
+	Timespan fromYearZero(year_t year, month_t month, date_t day) {
+		int64_t daysElapsed = year * DAYS_IN_YEAR;
+		size_t daysSinceMonthStart = (day == 0) ? 0 : day - 1; //Underflow protection
+		for (size_t x = 0; x < month && x < MONTHS_IN_YEAR; x++) {
+			daysElapsed += DAYS_IN_MONTH[x];
+		}
+		daysElapsed += daysSinceMonthStart;
+		return Timespan(daysElapsed);
+	}
+
+	//This function is not set up to handle dates before Year 0
+	YearMonthDayBundle toGregorianDate(Timespan const& timespan) {
+		year_t year = 0;
+		month_t month = 0;
+		date_t day = 0;
+
+		if (timespan >= 0) {
+			year = timespan.days / DAYS_IN_YEAR;
+			int64_t remainingDays = timespan.days % DAYS_IN_YEAR;
+
+			for (size_t x = 0; x < MONTHS_IN_YEAR && remainingDays >= DAYS_IN_MONTH[x]; x++) {
+				remainingDays -= DAYS_IN_MONTH[x];
+				month++;
+			}
+
+			//Corrects month and day to be 1-indexed
+			month++;
+			day++;
+		}
+		return std::make_tuple(year, month, day);
+	}
+
+
+	Date::Date(Timespan const& timespan) : ts(timespan) { updateDate(ts); }
+
+	Date::Date(year_t year, month_t month, date_t day) {
+		ts = fromYearZero(year, month, day);
+		updateDate(ts);
+	}
+
+	void Date::updateDate(Timespan const& timespan) {
+		gregorianDate = toGregorianDate(timespan);
+	}
+
+	size_t Date::getDay()   const { return std::get<2>(gregorianDate); }
+	size_t Date::getMonth() const { return std::get<1>(gregorianDate); }
+	size_t Date::getYear()  const { return std::get<0>(gregorianDate); }
+
+	bool Date::operator<  (Date const& other) const { return ts <  other.ts; }
+	bool Date::operator>  (Date const& other) const { return ts >  other.ts; }
+	bool Date::operator<= (Date const& other) const { return ts <= other.ts; }
+	bool Date::operator>= (Date const& other) const { return ts >= other.ts; }
+	bool Date::operator== (Date const& other) const { return ts == other.ts; }
+	bool Date::operator!= (Date const& other) const { return ts != other.ts; }
+
+	Date Date::operator+ (Timespan timespan) const { return Date(ts + timespan); }
+	Timespan Date::operator- (Date const& other) const { return ts - other.ts; }
+
+	Date& Date::operator+= (Timespan const& timespan) {
+		ts += timespan;
+		updateDate(ts);
+		return *this;
+	}
+	Date& Date::operator-= (Timespan const& timespan) {
+		ts -= timespan;
+		updateDate(ts);
+		return *this;
+	}
+	Date Date::operator++ (int) {
+		Date oldCopy = *this;
+		(*this) += 1;
+		return oldCopy;
+	}
+
+	Date::operator std::string() const {
+		std::stringstream ss;
+		ss << getYear() << '.' << getMonth() << '.' << getDay();
+		return ss.str();
+	}
+	std::ostream &operator<<(std::ostream &out, Date const& date) {
+		return out << static_cast<std::string>(date);
+	}
+}
