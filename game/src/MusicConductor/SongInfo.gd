99c99ce56bb3e73e65ff70352d4c9eef3d7f0f86
Spartan322 <Megacake1234@gmail.com>
2023-03-08 17:15:15 -0500

    Fix up MusicPlayer for future work Add music import files Rename MusicUIController to MusicPlayer Adjust MusicPlayer node position, offset, and anchors Adjust MusicConductor, MusicPlayer, and SongInfo style to conform with other GDScript files Correct capability for MusicPlayer to desync from the MusicConductor Adjusted MusicPlayer button text to use media player unicode symbols Adjust MusicPlayer to correct mouse filter problems Adjust MusicPlayer to appear more consistent Correct lack of path_join use in SongInfo Correct mouse filter problems in OptionsMenu

--- game/src/MusicConductor/SongInfo.gd
+++ game/src/MusicConductor/SongInfo.gd
@@ -1,11 +1,11 @@
 extends Node
 class_name SongInfo
 
-var fullyQualifiedPath : String = ""
-var readableName : String = ""
-var loadedStream : Resource
+var song_path : String = ""
+var song_name : String = ""
+var song_stream : Resource
 
 func _init(dirname:String, fname:String):
-		fullyQualifiedPath = dirname + fname
-		readableName = fname.get_basename().replace("_", " ")
-		loadedStream = load(fullyQualifiedPath)
+		song_path = dirname.path_join(fname)
+		song_name = fname.get_basename().replace("_", " ")
+		song_stream = load(song_path)
