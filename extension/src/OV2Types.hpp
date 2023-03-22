60430d05582d079086aaf9bb4228612d34871e91
ClarkeCode <clarke.john.robert@gmail.com>
2023-03-22 10:56:46 -0400

    Added type header

---
+++ extension/src/OV2Types.hpp
@@ -0,0 +1,12 @@
+#pragma once
+#include <cstdint>
+#include <string>
+
+namespace OpenVic2 {
+	using int_t = int64_t;
+	using unsigned_t = uint64_t;
+	using str_t = std::string;
+	
+	//TODO: replace with an implementation of fixed-point numbers
+	using fixed_t = double;
+}
