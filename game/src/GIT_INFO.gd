9768b1442ae780d528e0e236a05eb21a406e5513
Spartan322 <Megacake1234@gmail.com>
2025-05-15 19:24:34 -0400

    Fix GitInfo displays always being incorrect Move GitInfo retrieval to GDExtension Add .gdignore to game/export

--- game/src/GIT_INFO.gd
+++
@@ -1,9 +0,0 @@
-### IMPORTANT: IF LOCATION IS CHANGED, PLEASE UPDATE IN addon/openvic-plugin/ReleaseExportEditorPlugin
-class_name _GIT_INFO_
-extends RefCounted
-
-
-const commit_hash : StringName = &"0000000000000000000000000000000000000000"
-const short_hash : StringName = &"0000000"
-const tag : StringName = &"<tag missing>"
-const release_name : StringName = &"<release name missing>"
