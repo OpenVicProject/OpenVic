decbe2e127857d6fa4781e6304fabcecc7a63285
wvpm <24685035+wvpm@users.noreply.github.com>
2025-10-24 15:18:19 +0200

    UI for budget cuts

--- extension/src/openvic-extension/components/budget/SocialBudget.cpp
+++ extension/src/openvic-extension/components/budget/SocialBudget.cpp
@@ -7,6 +7,7 @@
 #include "openvic-extension/classes/GUINode.hpp"
 #include "openvic-extension/classes/GUIScrollbar.hpp"
 #include "openvic-extension/singletons/PlayerSingleton.hpp"
+#include "openvic-extension/utility/Utilities.hpp"
 
 using namespace OpenVic;
 
@@ -31,26 +32,39 @@ SocialBudget::SocialBudget(GUINode const& parent):
 		parent, "./country_budget/soc_stand_desc",
 		"SOCIAL_SPENDING","SOCIAL_DESC2"
 	);
-
-	if (budget_label != nullptr) {
-		budget_label->set_tooltip_string(
-			Utilities::format(
-				"%s\n--------------\n%s",
-				budget_label->tr("DIST_SOCIAL"),
-				budget_label->tr("SOCIAL_DESC2")
-			)
-		);
-	}
 }
 
 fixed_point_t SocialBudget::get_expenses() const {
 	return std::max(fixed_point_t::_0, -get_balance());
 }
 
+bool SocialBudget::was_budget_cut(CountryInstance const& country) const {
+	return country.get_was_social_budget_cut_yesterday();
+}
+
 fixed_point_t SocialBudget::calculate_budget_and_update_custom(
 	CountryInstance& country,
 	const fixed_point_t scaled_value
 ) {
+	if (budget_label != nullptr) {
+		budget_label->set_tooltip_string(
+			Utilities::format(
+				"%s\n--------------\n%s%s",
+				budget_label->tr("DIST_SOCIAL"),
+				was_budget_cut(country)
+					? budget_label->tr("EXPENSE_NO_AFFORD").replace(
+						Utilities::get_short_value_placeholder(),
+						Utilities::float_to_string_dp_dynamic(
+							country.get_actual_pensions_spending().load()
+							+ country.get_actual_unemployment_subsidies_spending().load()
+						)
+					) + "\n"
+					: "",
+				budget_label->tr("SOCIAL_DESC2")
+			)
+		);
+	}
+
 	const fixed_point_t pensions = scaled_value * country.get_projected_pensions_spending_unscaled_by_slider_untracked();
 	pensions_label.set_text(
 		Utilities::cash_to_string_dp_dynamic(pensions)
